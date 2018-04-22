bee2983523ed1141b3eed46df0685a8cc3edfd3e
William Zhu <wbzhu@ucsd.edu>
2018-04-22 16:26:42 -0700

    added dummy graph, js files...

---
+++ static_files/js/script.js
@@ -0,0 +1,38 @@
+
+// ES6 style or old javascript?
+
+$(function() {
+  const graphExample = (data, type, title) => {
+    var trace = setTrace(data[0], data[1], 'rgb(55, 128, 191)');
+    
+    data = [trace];
+    var layout = {
+      title: title,
+      xaxis: {
+        title: 'x'
+      },
+      yaxis: {
+        title: 'y'
+      }
+    };
+    
+    Plotly.newPlot("graph-" + type, data, layout);
+  }
+  
+  const setTrace = (x, y, color) => {
+    return {
+      x: x,
+      y: y,
+      type: 'bar',
+      line: {
+        color: color
+      }
+    };
+  }
+  
+  const xData = ['a', 'the', 'hello', 'thing', 'game'];
+  const yData = [50, 100, 5, 20, 10];
+  const data = [xData, yData];
+  
+  graphExample(data, 'example', 'Example Graph');
+});
